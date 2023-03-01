729ba7befce01d532eb9881e9dcb601e5d77f01d
이치훈 <dlclgns152615@gmail.com>
2023-03-02 02:02:02 +0900

    test

---
+++ Basic_1_Constraints/Basic_1_Constraints/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Basic_1_Constraints
+//
+//  Created by 이치훈 on 2023/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "test"
+        
+        
+    }
+    
+    
+    
+    @IBAction func testButton(_ sender: Any) {
+        print("test")
+    }
+    
+
+}
+
---
+++ Basic_1_Constraints/Basic_1_ConstraintsUITests/Basic_1_ConstraintsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Basic_1_ConstraintsUITestsLaunchTests.swift
+//  Basic_1_ConstraintsUITests
+//
+//  Created by 이치훈 on 2023/02/27.
+//
+
+import XCTest
+
+final class Basic_1_ConstraintsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
